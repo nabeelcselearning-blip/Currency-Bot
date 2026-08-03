0c58175967773ebe9203b02482a22b28f4f8f266
Nabeel Islam <nabeel.cse.learning@gmail.com>
2026-08-04 00:17:57 +0530

    find currency stored location Getting rate of 1 USD in INR

--- currency_bot.py
+++ currency_bot.py
@@ -5,9 +5,23 @@ def get_exchange_rate():
     # Our new backup API URL. This gets all rates for USD.
     url = "https://open.er-api.com/v6/latest/USD"
 
+    # 1. Send a 'GET' request to the server
     response = requests.get(url)
 
-    print(response.text)
+    # 2. Print the raw text the server sends back
+    # print(response.text)
+
+    # Lets do parsing
+    # 1. Convert the raw JSON text into a Python dictionary
+    data = response.json()
+    # 2. Extract our specific currency.
+    # Look closely at the data, the currencies are stored inside a 'rates' container.
+    inr_rate = data["rates"]["INR"]
+
+    print(f"Success! 1 USD is currently: {inr_rate} INR")
+
+    # We return the value so we can use it in the next step
+    return inr_rate
 
 
 get_exchange_rate()
