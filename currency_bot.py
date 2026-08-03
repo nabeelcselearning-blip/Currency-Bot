d496bb436931c7df18eceb21eb96e0c27586fa3e
Nabeel Islam <nabeel.cse.learning@gmail.com>
2026-08-04 00:08:25 +0530

    First Step:- -> Import the requests library. -> Send an HTTP GET request to a REST API. -> Receive a response from the server. -> Display the raw JSON response returned by the API.

--- currency_bot.py
+++ currency_bot.py
@@ -2,21 +2,12 @@ import requests
 
 
 def get_exchange_rate():
+    # Our new backup API URL. This gets all rates for USD.
     url = "https://open.er-api.com/v6/latest/USD"
+
     response = requests.get(url)
 
-    # 1. Convert the raw JSON text into a Python dictionary
-    data = response.json()
-
-    # 2. Extract our specific currency.
-    # If you look closely at the data, the currencies are stored inside a 'rates' container.
-    inr_rate = data["rates"]["INR"]
-
-    print(f"Success! 1 USD is currently: {inr_rate} INR")
-
-    # We return the value so we can use it in the next step
-    return inr_rate
+    print(response.text)
 
 
-# Run it to test
 get_exchange_rate()
